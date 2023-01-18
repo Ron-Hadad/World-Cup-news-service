d5848fd6864a7cd66a1843b5452f633d2de11093
GuyG1996 <guyginat0@gmail.com>
2023-01-18 10:14:11 +0200

    smallChange

--- SPL231-Assignment3-student-template-2/client/src/StompProtocol.cpp
+++ SPL231-Assignment3-student-template-2/client/src/StompProtocol.cpp
@@ -140,44 +140,28 @@ std::string StompProtocol::PrintSummary(std::string messege){
     std::string data = team_a_name + "vs" + team_b_name + "\n";
     data += "Game stats:\n";
     data += "General stats:\n";
-<<<<<<< HEAD
     for(unsigned int i = 0; i < reports.size(); i++){
-=======
-    for(unsigned i = 0; i < reports.size(); i++){
->>>>>>> 70afea57cade1bc4dec90886a21ad66fdbc9e7be
         map<std::string, std::string> game_updates = reports[i].get_game_updates();
         for(auto const& it : game_updates){
             data += it.first + ":" + it.second + "\n";
         }  
     }
     data += team_a_name + "stats:\n";
-<<<<<<< HEAD
     for(unsigned int i = 0; i < reports.size(); i++){
-=======
-    for(unsigned i = 0; i < reports.size(); i++){
->>>>>>> 70afea57cade1bc4dec90886a21ad66fdbc9e7be
         map<std::string, std::string> team_a_updates = reports[i].get_team_a_updates();
         for(auto const& it : team_a_updates){
             data += it.first + ":" + it.second + "\n";
         }  
     }
     data += team_b_name + "stats:\n";
-<<<<<<< HEAD
     for(unsigned int i = 0; i < reports.size(); i++){
-=======
-    for(unsigned i = 0; i < reports.size(); i++){
->>>>>>> 70afea57cade1bc4dec90886a21ad66fdbc9e7be
         map<std::string, std::string> team_b_updates = reports[i].get_team_b_updates();
         for(auto const& it : team_b_updates){
             data += it.first + ":" + it.second + "\n";
         }  
     }
     data += team_a_name + "game event reports:\n";
-<<<<<<< HEAD
     for(unsigned int i = 0; i < reports.size(); i++){
-=======
-    for(unsigned i = 0; i < reports.size(); i++){
->>>>>>> 70afea57cade1bc4dec90886a21ad66fdbc9e7be
         int time = reports[i].get_time();
         std::string name = reports[i].get_name();
         std::string description = reports[i].get_discription();
